610c08d704d7b8e6af0fee6c75939b18a133106e
vasasharath <sharathvasa@gmail.com>
2018-12-27 14:44:01 -0500

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, FlatList } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import { MailComposer } from 'expo';
 
 class ContactUs extends Component {
     constructor(props) {
@@ -9,33 +10,47 @@ class ContactUs extends Component {
         this.state = {
         };
     }
-    
+
     static navigationOptions = {
         title: ''
     }
 
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
+
     render() {
-        return(
+        return (
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
                 <Card title="Contact Information">
-                    <Text style = {{margin: 10}}>
+                    <Text style={{ margin: 10 }}>
                         121, Clear Water Bay Road
                     </Text>
-                    <Text style = {{margin: 10}}>
+                    <Text style={{ margin: 10 }}>
                         Clear Water Bay, Kowloon
                     </Text>
-                    <Text style = {{margin: 10}}>
+                    <Text style={{ margin: 10 }}>
                         HONG KONG
                     </Text>
-                    <Text style = {{margin: 10}}>
+                    <Text style={{ margin: 10 }}>
                         Tel: +852 1234 5678
                     </Text>
-                    <Text style = {{margin: 10}}>
+                    <Text style={{ margin: 10 }}>
                         Fax: +852 8765 4321
                     </Text>
-                    <Text style = {{margin: 10}}>
+                    <Text style={{ margin: 10 }}>
                         Email:confusion@food.net
                     </Text>
+                    <Button
+                        title="Send Email"
+                        buttonStyle={{ backgroundColor: "#512DA8" }}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={this.sendMail}
+                    />
                 </Card>
             </Animatable.View>
         );
